70db4a9d6d710cc0576e4f32e1d5aa30adacdb2c
Danilo Santiago <danilo.santiago.20241@poli.ufrj.br>
2026-05-03 15:05:33 -0300

    inicial commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-100">
+    <View className="flex-1 items-center justify-center bg-green-500">
       <Text className="text-lg font-bold text-black">Tailwind OKK</Text>
     </View>
   );
