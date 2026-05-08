36fa2d110f4a1533aae3a1eea54871d937956ce2
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-05-08 14:46:20 -0300

    começando a splashscreen, foi mal danilo vou criar minha branch

---
+++ src/app/splash.tsx
@@ -0,0 +1,23 @@
+import {useRouter} from "expo-router"
+import { useEffect } from "react"
+import { View, Image } from "react-native"
+
+export default function splashScreen(){
+    const router = useRouter()
+
+
+useEffect(() => {
+    const timer = setTimeout(()=> {
+        router.replace("/")
+    }, 4000) // tempo em ms que a splash vai ficar visivel no app
+
+    return () => clearTimeout(timer)
+}, []);
+
+    return(
+        <View className="flex-1 items-center justify-center bg-white">
+            {/* meter uma animação wooooow aqui */}
+        </View>
+    )
+
+}
