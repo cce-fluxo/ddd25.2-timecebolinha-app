f70f9f611726df1591b791810d424aed531fc65e
Pedro Motta <163004710+xjott@users.noreply.github.com>
2026-05-22 11:00:56 -0300

    primeiro commit, mudando o api.ts pra ficar sem conflito de nomenclatura

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
 import { storage } from '../utils/storage';
+import Constants from 'expo-constants'
+
+// em dev, ele vai pegar o IP do servidor Expo automaticamente
+
+const getBaseUrl = ()=> {
+  const host = Constants.expoConfig?.hostUri?.split(':')[0];
+  if(host) return `http://${host}:3100`;
+  //fallback pra web/produção
+  return process.env.EXPO_PUBLIC_BASE_URL ?? 'http://localhost:3100';
+}
 
 const api = axios.create({
-    baseURL: process.env.EXPO_PUBLIC_BASE_URL,
+    baseURL: getBaseUrl(),
 });
+
+// essa parte ai de cima basicamente é pra fazer o IP deixar de ser hardcoded e o app funcionar em qualquer IP
 
 // Interceptor de REQUEST: injeta o token JWT em toda requisição autenticada
 api.interceptors.request.use(async (config) => {
@@ -39,6 +51,16 @@ export interface PacientePayload {
     }
 }
 
+export interface Paciente{
+  id:number
+  no_usuario: string
+  email_usuario: string
+  nu_celular: string
+  cpf:string
+  data_nascimento: string
+  genero: string
+}
+
 export async function criarPaciente(payload: PacientePayload) {
     const { data } = await api.post('/paciente', payload);
     return data;
@@ -46,13 +68,13 @@ export async function criarPaciente(payload: PacientePayload) {
 
 export async function login(email: string, senha_usuario: string) {
   const { data } = await api.post<{ access_token: string ; id:number }>('/auth/login', {
-    email: email,
+    email_usuario: email, // Tinha que chamar "email_usuario" , não email
     senha_usuario: senha_usuario,
   })
   return data
 }
 
-export async function getUsuario(id:number){
+export async function getMe(id:number){
   const {data} = await api.get(`/usuarios/unico/${id}`)
   return data
 }
