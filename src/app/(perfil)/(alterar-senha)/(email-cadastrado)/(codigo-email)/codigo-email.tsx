f40579852b58009f4af9533cd7c63243a0cfc21c
pedronvmotta <pedronaves.20241@poli.ufrj.br>
2026-05-23 10:11:28 -0300

    primeiro popup adicionado

--- src/app/(perfil)/(alterar-senha)/(email-cadastrado)/(codigo-email)/codigo-email.tsx
+++ src/app/(perfil)/(alterar-senha)/(email-cadastrado)/(codigo-email)/codigo-email.tsx
@@ -1,7 +1,6 @@
 import { View, Image, Text } from "react-native";
 import { TouchableOpacity } from "react-native";
 import InputBox from "@/src/components/InputBox";
-import BotaoDeContinuar from "@/src/components/BotaoDeContinuar";
 import { useRouter } from "expo-router";
 import { useState} from "react";
 import { useLocalSearchParams } from "expo-router";
@@ -13,6 +12,7 @@ export default function CodigoEmail(){
     const [codigo, setCodigo] = useState("")
     const [erro, setErro] = useState("")
     const [carregando, setCarregando] = useState(false)
+    const [popupVisivel, setPopupVisivel] = useState(false)
 
     async function confirmarCodigo(){
         try{
@@ -31,6 +31,8 @@ export default function CodigoEmail(){
 
         try{
             await enviarCodigoEmail(email)
+            setPopupVisivel(true)
+            setTimeout(()=> setPopupVisivel(false), 3000)
         }catch(e:any){
             setErro(e.message ?? "Erro ao reenviar código")
         }
@@ -63,7 +65,6 @@ export default function CodigoEmail(){
                 {/*vou deixar o onPress dummy aqui porque ele iria só pra um pop up e isso eu vou fazer depois*/}
                 <TouchableOpacity onPress={reenviarCodigo} className="flex text-indigo-600 font-lato-bold text-[13px]">Reenviar código</TouchableOpacity>
             </View>
-
             <View className="ml-24 mt-8">
                 <TouchableOpacity onPress={confirmarCodigo} disabled={carregando}>
                 <View style={{flex:1, width:'100%', borderRadius: 8}}>
@@ -73,7 +74,13 @@ export default function CodigoEmail(){
         </View>
                 </TouchableOpacity>
             </View>
-        </View>
+            {/*antes do último /view, vou colocar o meu popup, aqui eu meio que vou criar um pq ele vai sumir depois de um tempo ent não da pra usar a tag Modal*/}
+            {popupVisivel && (
+                <View className="absolute bottom-2 self-center bg-white px-5 py-2 rounded-lg shadow">
+                    <Text className="text-indigo-600 font-lato-regular">Código reenviado</Text>
+            </View>
+            )}
+            </View>
     )
 
 }
